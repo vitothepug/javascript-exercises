ee7e4b2ccac1c8db418b308efa5a5d58abe49d55
vitothepug <beren6@me.com>
2022-07-03 16:48:47 -0500

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
+let repeatString = function(string, num) {
+    let newString=''
+    if (num < 0) return 'ERROR';
+    for (var i = 1; i <= num; ++i) {
+       newString += string
+    }
+    
+    return (newString);
 };
+// console.log(repeatString("hey",10))
+// repeatString("hey",1)
+string=''
 
 // Do not edit below this line
 module.exports = repeatString;
