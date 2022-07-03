da89cbdb768a88f30d42aae47e9c3cbd36ed475d
vitothepug <beren6@me.com>
2022-07-03 17:04:33 -0500

    ctof

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+let tempC
+let tempF
 
+const ftoc = function(tempF) {
+  let tempC=''
+  tempC = ((tempF-32)/1.8)
+  tempC = tempC.toFixed(1);
+  return Number(tempC)
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF=''
+  tempF = ((tempC*1.8)+32);
+  tempF = tempF.toFixed(1);
+  return Number(tempF)
 };
 
 // Do not edit below this line
